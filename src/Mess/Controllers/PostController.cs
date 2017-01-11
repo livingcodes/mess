17929f451fd08ee693052347ee1aa93496f67658
casey <case.joyce@gmail.com>
2017-01-11 03:52:18 +0000

    Start web api example /api/posts

---
+++ src/Mess/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using Mess.Models;
+using Mess.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Mess.Controllers
+{
+public class PostController : Controller
+{
+    public PostController(IPostService service) {
+        this.service = service;
+    }
+    IPostService service = null;
+
+    [Route("api/posts")]
+    public List<Post> Get() {
+        return service.Get();
+    }
+}
+}
