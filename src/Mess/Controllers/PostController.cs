4920d25304fb4a765adc4d0af86dc33861eaac77
casey <case.joyce@gmail.com>
2017-01-14 04:15:12 +0000

    add api/post/{id}

--- src/Mess/Controllers/PostController.cs
+++ src/Mess/Controllers/PostController.cs
@@ -2,6 +2,7 @@
 using Mess.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Mess.Controllers
 {
@@ -12,9 +13,14 @@ public class PostController : Controller
     }
     IPostService service = null;
 
-    [Route("api/posts")]
+    [Route("api/[controller]s")]
     public List<Post> Get() {
         return service.Get();
+    }
+
+    [Route("api/[controller]/{id}")]
+    public Post Get(int id) {
+        return service.Get().FirstOrDefault(p => p.Id == id);
     }
 }
 }
--- src/Mess/Services/PostService.cs
+++ src/Mess/Services/PostService.cs
@@ -16,24 +16,21 @@ public class PostService : IPostService
 
     List<Post> query() {
         var posts = new List<Post>();
-        posts.Add(new Post()
-        {
+        posts.Add(new Post() {
             Id = 1,
             Title = "WebForms Fluent HTTP Request",
             Body = "Request.Header(\"user\", \"me\");",
             FriendlyUrlTitle = "webforms-fluent-http-request",
             PublishDate = new DateTime(2016, 12, 7)
         });
-        posts.Add(new Post()
-        {
+        posts.Add(new Post() {
             Id = 2,
             Title = "Pony Lang Land",
             Body = "pony ...",
             FriendlyUrlTitle = "pony-lang-land",
             PublishDate = new DateTime(2016, 12, 9)
         });
-        posts.Add(new Post()
-        {
+        posts.Add(new Post() {
             Id = 3,
             Title = "ASP.NET Core",
             Body = "Action Result Options",
